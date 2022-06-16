405aea43ac3fc57d4b63c82216fc092f4e5def5c
junglemaodev <adenilton.barroso@hotmail.com>
2022-06-16 04:32:53 -0400

    Adicionando a revisão do Curso Primeiros Passos com .NET

---
+++ Primeiros_Passos_com_Dotnet/Revisao/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno[] alunos = new Aluno[5];
+            int idxAluno = 0;
+
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Informe o nome do aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+                        if (Decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve ser decimal.");
+                        }
+
+                        alunos[idxAluno] = aluno;
+                        idxAluno++;
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        foreach (var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
+                        Console.ReadLine();
+                        break;
+
+                    case "3":
+                        decimal notaGeral = 0;
+                        int qtdeAlunos = 0;
+                        for (int i = 0; i < 5; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaGeral += alunos[i].Nota;
+                                qtdeAlunos++; 
+                            }
+                        }
+
+                        decimal media = notaGeral/qtdeAlunos;
+
+                        Console.WriteLine($"MEDIA GERAL: {media}");
+                        Console.ReadLine();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException("Opção inválida!");
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno.");
+            Console.WriteLine("2 - Listar alunos.");
+            Console.WriteLine("3 - Calcular média geral.");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine("");
+
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+        }
+    }
+}
